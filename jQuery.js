c1ec3f3518f8360ac75d35f10790231e427a03bc
Kato819 <116700163+Kato819@users.noreply.github.com>
2022-11-15 14:46:28 +0900

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -1,24 +1,8 @@
 $(function() {
-
-    var btn = 'close';
-
     $('.works-list-item:nth-last-child(-n+4)').hide();
-
-    for(a=1;a=1;a = a-1){
-        $('.view-more-btn').click(function(){
-
-            if(btn = 'close'){
-                $('.works-list-item:nth-last-child(-n+4)').slideDown();
-                $('.btn').text('close');
-                a = a+1;
-            }else{
-                $('.works-list-item:nth-last-child(-n+4)').slideUp();
-                $('.btn').text('view more');
-                a = a+1;
-            }
-
-        });
-    }
-
-
+    $('.view-more-btn').click(function(){
+        $('.works-list-item:nth-last-child(-n+4)').slideDown();
+        $('.btn').text('close');
+        a = a+1;
+    });
 })
