b9b0dca147a0071c3d36a5447f779d392e52ce36
Kato819 <116700163+Kato819@users.noreply.github.com>
2022-11-15 14:46:39 +0900

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -3,6 +3,5 @@ $(function() {
     $('.view-more-btn').click(function(){
         $('.works-list-item:nth-last-child(-n+4)').slideDown();
         $('.btn').text('close');
-        a = a+1;
     });
 })
